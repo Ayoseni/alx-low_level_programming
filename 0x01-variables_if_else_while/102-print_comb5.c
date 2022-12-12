9e47e03409dd4c3066405d1e658fca5a2f6ed761
Ayoseni <adekoyaseni@gmail.com>
2022-12-12 09:39:09 -0800

    ptint combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry value
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	int num;
+	int num1;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (num1 = num + 1; num1 < 10; num1++)
+		{
+			putchar ((num / 10) + '0');
+			putchar ((num % 10) + '0');
+			putchar (' ');
+			putchar ((num1 / 10) + '0');
+			putchar ((num1 % 10) + '0');
+
+			if (num == 98 && num1 == 99)
+				continue;
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
