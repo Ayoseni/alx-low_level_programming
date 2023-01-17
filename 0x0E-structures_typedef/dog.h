73a67dbf2f0f0cf3623fb699a43835556f900982
Ayoseni <adekoyaseni@gmail.com>
2023-01-17 13:16:42 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - data type of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+#endif
